58afb0da92da826f540e63c2d049e5d1c4617841
alidauda <alidauda14@gmail.com>
2022-08-09 23:42:15 +0100

    usemutate not working

--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -1,26 +1,42 @@
-import { NextPage } from "next";
-import { useState } from "react";
-import graphqlRequestClient from "../src/clients/graphqlResquest";
-import { RegisterMutation, RegisterMutationVariables,  useRegisterMutation } from "../src/generated/graphql";
+import { Field, Form, Formik } from 'formik';
+import { NextPage } from 'next';
+import { useState } from 'react';
+import graphqlRequestClient from '../src/clients/graphqlResquest';
+import {
+  RegisterMutation,
+  RegisterMutationVariables,
+  useRegisterMutation,
+} from '../src/generated/graphql';
 
-const Register:NextPage=()=>{
-    const{mutate}=useRegisterMutation(graphqlRequestClient,{
-        onSuccess: (data: RegisterMutation, _variables: RegisterMutationVariables, _context: unknown) => {
-           
-            return console.log('mutation data', data);
-          },
+const Register: NextPage = () => {
+  const { mutate } = useRegisterMutation<RegisterMutation>(graphqlRequestClient, {
+    onSuccess: (
+      data: RegisterMutation,
+      _variables: RegisterMutationVariables,
+      _context: unknown
+    ) => {
+      return console.log('mutation data', data);
+    },
+  });
+  const [email, setEmail] = useState('');
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={async (values, { setErrors }) => {
+        const response= mutate(values)
+if(response.){
 
-    })
-    const [email,setEmail]=useState("")
-    return(
-        <div>
-        <input type={"email"} placeholder="email" onChange={(e)=>setEmail(e.target.value)}/>
-        <input type={"password"} placeholder="password"/>
-        <button onClick={(e)=>{
-            mutate({  email: email, password:email })
-        }}>submit</button>
-        </div>
-    )   
 }
+      }}
+    >
+      <Form>
+        <label htmlFor='email'>Email</label>
+        <Field name='email' type='email' />
+        <label htmlFor='password'>Password</label>
+        <Field name='firstName' type='password' />
+      </Form>
+    </Formik>
+  );
+};
 
-export default Register ;
+export default Register;
